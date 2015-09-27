6bf700e46cd03a060ac6ea5537261db7f2925e5f
Ruben <rcaldeco7@alumnes.ub.edu>
2015-09-27 23:50:16 +0200

    Revert "Revert "Primeres probes AI""
    This reverts commit de2dc94402927bab5f742685e145e540c4770e74.

---
+++ Enemy AI/Assets/AIScripts/autoMovement.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class autoMovement : MonoBehaviour {
+	void Update() {
+		transform.Translate(Vector3.forward * Time.deltaTime);
+		transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+	}
+}
