4169d3f6625808597dd0e53e4188b4f90a69cf93
joanbm <joanbrugueram@gmail.com>
2015-09-30 23:47:10 +0200

    Crear implementación básica del código de selección de elementos RTS, a unir al trabajo de instanciación de clases pendiente de subir.

---
+++ UnityTestProjectIssue7/Assets/CameraSelectionBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Manages the RTS element selection events.
+/// This script must be assigned to the camera of the scene in which the user can select RTS elements.
+/// </summary>
+public class CameraSelectionBehaviour : MonoBehaviour {	
+	/// <summary>
+    /// Update function, called every frame.
+    /// </summary>
+	void Update () {
+        if (Input.GetMouseButtonDown(0)) // If the left mouse button is clicked...
+        {
+            RTSElement objectRtsElement = null;
+
+            // Cast a ray in the direction clicked by the user to detect the currently clicked element,
+            // and if it is a RTS element, then save its reference
+            Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                GameObject objectHit = hit.collider.gameObject;
+                objectRtsElement = objectHit.GetComponent<RTSElement>();
+            }
+
+            // Call the class to change the currently selected RTS element.
+            // Note that this will be null if the user clicked on nothing or a non-RTS element object.
+            RTSElement.ChangeSelection(objectRtsElement);
+        }
+    }
+}
---
+++ UnityTestProjectIssue7/Assets/RTSElement.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class RTSElement : MonoBehaviour {
+    /// <summary>
+    /// Reference to the currently selected RTS element.
+    /// </summary>
+    public static RTSElement SelectedElement { get; set; }
+
+    /// <summary>
+    /// Called when the RTS element selection begins.
+    /// </summary>
+    protected virtual void OnSelectionStart()
+    {
+        Debug.Log("RTS element selected: " + name);
+    }
+
+    /// <summary>
+    /// Called when the RTS element selection end.
+    /// </summary>
+    protected virtual void OnSelectionEnd()
+    {
+        Debug.Log("RTS element unselected: " + name);
+    }
+
+    /// <summary>
+    /// Changes the currently selected RTS element, or unselects it.
+    /// </summary>
+    /// <param name="objectRtsElement">The new RTS element to select, or null to unselect any element.</param>
+    internal static void ChangeSelection(RTSElement objectRtsElement)
+    {
+        if (SelectedElement != null)
+        {
+            SelectedElement.OnSelectionEnd();
+        }
+
+        SelectedElement = objectRtsElement;
+
+        if (SelectedElement != null)
+        {
+            SelectedElement.OnSelectionStart();
+        }
+    }
+}
