81aafee394b1255fe22b10c8726c1bdf64e624c6
Ruben <Ruben@Lenovo.local>
2015-10-06 19:56:51 +0200

    IA de l'enemic

---
+++ Map/Assets/Scripts/IA/RDA.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+// Rango De Ataque
+var ataque : IA;
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter(other: Collider){
+	if(other.gameObject.tag == "mina"||other.gameObject.tag=="arbol" || other.gameObject.tag=="comida"){
+		ataque.estado = 3;
+	}
+}
+
+function OnTriggerExit(other: Collider){
+	if(other.gameObject.tag == "mina"||other.gameObject.tag=="arbol" || other.gameObject.tag=="comida"){
+		ataque.estado = 2;
+	}
+}
