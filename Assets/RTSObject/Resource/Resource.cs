0e209ab324a548da56189faeca7dc646b235f404
Daniel Aparisi <danielapa_ellider@hotmail.com>
2015-10-06 16:43:14 +0200

    Update all RTSObjects
    Updated RTSObject class.
    Updated Building class.
    Updated Resource class.
    Updated Unit class and merged with ClickToMove.
    Added UnitAC Animator Controller.

--- Assets/RTSObject/Resource/Resource.cs
+++ Assets/RTSObject/Resource/Resource.cs
@@ -3,17 +3,45 @@ using System.Collections;
 
 public class Resource : RTSObject {
 
-    public enum ResourceType { Money, Power, Ore, Wood }
-    public float capacity;
+    public enum ResourceType { Money, Power, Ore, Wood, Unknown }    // Declarem els tipus de recursos
+    public float capacity;                          // Capacitat del recurs
 
-    protected float amountLeft;
-    protected ResourceType resourceType;
-    
-    protected override void Start () {
-	
-	}
-    
-    protected override void Update () {
-	
-	}
+    protected float amountLeft;                     // Indica la cantitat que queda del recurs
+    protected ResourceType resourceType;            // Indica el tipus de recurs
+
+    /*** Metodes per defecte de Unity ***/
+
+    protected override void Start()
+    {
+        base.Start();
+        resourceType = ResourceType.Unknown;
+        amountLeft = capacity;
+    }
+
+    /*** Metodes publics ***/
+
+    // Metode per extreure una quantitat al recurs
+    public void Remove(float amount)
+    {
+        amountLeft -= amount;
+        if (amountLeft < 0) amountLeft = 0;
+    }
+
+    // Metode per obtenir si el recurs es buit o no
+    public bool isEmpty()
+    {
+        return amountLeft <= 0;
+    }
+
+    // Metode per obtenir el tipus de recurs
+    public ResourceType GetResourceType()
+    {
+        return resourceType;
+    }
+
+    // Metode per calcular la vida actual del recurs
+    protected override void CalculateCurrentHealth()
+    {
+        healthPercentage = amountLeft / capacity;
+    }
 }
