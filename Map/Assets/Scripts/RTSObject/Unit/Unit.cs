ef25badbc1b0d7d20eb0fe6c0ffb57ac054a31a7
joanbm <joanbrugueram@gmail.com>
2015-10-12 11:46:18 +0200

    Fix and simplify animation handling in the Unit movement code.

--- Map/Assets/Scripts/RTSObject/Unit/Unit.cs
+++ Map/Assets/Scripts/RTSObject/Unit/Unit.cs
@@ -35,12 +35,10 @@ public class Unit : RTSObject
     protected override void Update()
     {
         base.Update();
-        if (path == null || currentWaypoint >= path.vectorPath.Count)
-        {
-            moving = false;
-            return;
-        }
-        else
+
+        // If the unit is currently moving, call the function to update
+        // the position in the path the unit is following
+        if (path != null && currentWaypoint < path.vectorPath.Count)
         {
             moveToPosition();
         }
@@ -69,9 +67,16 @@ public class Unit : RTSObject
         return true;
     }
 
+    /// <summary>
+    /// Tells the unit to move to the given position, by generating and
+    /// following a route to the desired position.
+    /// </summary>
+    /// <param name="target">The position we want the unit to move to.</param>
     public void setNewPath(Vector3 target)
     {
-        
+        // We're starting movement, so start the walking animation
+        moving = true;
+
         targetPosition = target;
         seeker.StartPath(transform.position, targetPosition, OnPathComplete);
     }
@@ -89,21 +94,30 @@ public class Unit : RTSObject
 
     private void moveToPosition()
     {
-        moving = true;
+        // Set the rotation of the model to point in the direction of the target
         Quaternion newRotation = Quaternion.LookRotation(targetPosition - transform.position);
-
         newRotation.x = 0f;
         newRotation.z = 0f;
         transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * 10);
 
+        // Tell the controller to move in the straight-line direction
+        // between the current position and the waypoint
         Vector3 dir = (path.vectorPath[currentWaypoint] - transform.position).normalized;
         dir = dir * moveSpeed * Time.deltaTime;
         controller.SimpleMove(dir);
 
+        // Have we reached the waypoint in the path?
         if (Vector3.Distance(transform.position, path.vectorPath[currentWaypoint]) < nextWaypointDistance)
         {
+            // Advance to the next waypoint
             currentWaypoint++;
-            return;
+
+            // If we have reached the last waypoint in the path,
+            // then we need to stop the walking animation
+            if (path.vectorPath.Count == currentWaypoint)
+            {
+                moving = false;
+            }
         }
     }
 }
