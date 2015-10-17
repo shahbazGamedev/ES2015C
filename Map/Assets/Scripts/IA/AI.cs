f9605286452f5e0f4245cb44548b4f67eda8d8b8
sseerr20 <jojomasira@gmail.com>
2015-10-17 17:39:56 +0200

    -Es posen els recursos aleatoriament al mapa, de moment només arbres -La ia ja sap quan construir, les distancies poden estar malament, falta testeig

---
+++ Map/Assets/Scripts/IA/AI.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class AI : MonoBehaviour {
+
+    // Use this for initialization
+    List<Unit> civils;
+    //List<GameObject> townCenters;
+    int townCenters;
+    List<GameObject> soldiers;
+    Vector3 position;
+    Unit civil;
+    bool building=false;
+    int i;
+
+    void Start () {
+        position = new Vector3(110,0,200);
+        //civil= Instantiate(Resources.Load("yamato_civil", typeof(GameObject)), position, Quaternion.identity) as GameObject;
+        civil = Instantiate(Resources.Load("yamato_civil", typeof(GameObject)), position, Quaternion.identity) as Unit;
+        civils.Add(civil);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        buildTownCenter();
+        build("farm","food");
+        build("mine", "metal");
+        build("woodCutter", "tree");
+    }
+
+
+    void buildTownCenter() {
+        //Si no tinc cap centro urbano, tenir-ne una ha de ser la meva prioritat
+        if (townCenters == 0) {
+
+            while ( i < civils.Count && building == true) // Loop with for.
+            {
+                // civilian.buildTownCenter();  //Encara no està implementat
+                townCenters++;
+                building = true;
+            }
+
+
+        }
+    }
+
+    void build(String building, String resource) {
+
+        GameObject food;
+        GameObject farm;
+        GameObject townCenter;
+        Vector3 buildingPosition;
+        Vector3 unitPosition;
+        Vector3 closestDistance;
+        float totalDist;
+
+        foreach (Unit civilian in civils)
+        {
+            unitPosition = civilian.transform.position; //Agafo la posicio del civil
+            townCenter = civilian.FindClosest("townCenter"); //Retorna el TownCenter més proper
+            buildingPosition = townCenter.transform.position;//Agafo la posicio
+            closestDistance = new Vector3(unitPosition.x - buildingPosition.x,0,unitPosition.z - buildingPosition.z);
+            totalDist = (float)Math.Sqrt(closestDistance.x*closestDistance.x+closestDistance.z*closestDistance.z);//i calculo  la distancia euclidania
+
+            if (totalDist > 50) { //Si les distancia és més petita de 50 no val la pena anar a construir una farm, ja es pot anar al TownCenter
+                farm = civilian.FindClosest(building);
+                buildingPosition = farm.transform.position;
+                closestDistance = new Vector3(unitPosition.x - buildingPosition.x, 0, unitPosition.z - buildingPosition.z);
+                totalDist = (float)Math.Sqrt(closestDistance.x * closestDistance.x + closestDistance.z * closestDistance.z);//i calculo  la distancia euclidania
+
+                if (totalDist>50) {
+                    food = civilian.FindClosest(resource);
+                    buildingPosition = food.transform.position;
+                    closestDistance = new Vector3(unitPosition.x - buildingPosition.x, 0, unitPosition.z - buildingPosition.z);
+                    totalDist = (float)Math.Sqrt(closestDistance.x * closestDistance.x + closestDistance.z * closestDistance.z);//i calculo  la distancia euclidania
+
+                    if (totalDist < 50) {
+                        //civilian.buildFarm();        Encara no esta implementat aquest mètode
+                    }
+
+                }
+            }
+
+        }
+    }
+
+}
---
+++ Map/Assets/Scripts/PlaceResources.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaceResources : MonoBehaviour {
+
+    // Use this for initialization
+    private int i,j,k; //s'utilitzen per recorrer el fors(PlaceForests)
+
+    int width = 500;//mides del mapa
+    int depth = 500;
+
+    Vector3 coords;
+
+    void Start () {
+        PlaceRandomResources(100,"arbolYamato1");
+        PlaceGroup(10,30,"arbolYamato1");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void PlaceRandomResources(int numberOfResources,string asset) {
+
+        for (i=0;i<numberOfResources;i++) {
+            double randomDepth = Random.Range(0, depth);
+            double randomWidth = Random.Range(0, width);
+            coords = new Vector3((float)randomWidth,-1.6f,(float)randomDepth);
+            Instantiate(Resources.Load(asset, typeof(GameObject)), coords, Quaternion.identity); //Crear l'arbre
+        }
+    }
+
+
+    void PlaceGroup(int numGroups, int maxElementsInGroup,string PrefabResource){
+
+       for (i = 0; i < numGroups; i++) //Numero de boscos que tindra el mapa
+       {
+            double randomDepth = Random.Range(0, depth); //agafa un numero aleatori, dins de les mides del mapa
+            double randomWidth = Random.Range(0, width);
+
+            coords = new Vector3((float)randomWidth, -1.6f, (float)randomDepth); //Es creen les coordenades a partir dels numeros anteriors
+
+            int numberOfElementsX = Random.Range(0, maxElementsInGroup);  //Numero aleatori entre 0 i el parametre(controla quans arbres i pot haver a un bosc)
+            int numberOfElementsY = Random.Range(0, maxElementsInGroup);
+
+            //Va reccorent per crear el bosc
+            for (j=0;j< numberOfElementsX; j++) { 
+            j = j + 2;// es per augmentar l'espai entre arbres
+                for (k=0;k< numberOfElementsY; k++) {
+                    coords = new Vector3((float)randomWidth+j, -1.6f, (float)randomDepth+k);
+                    if (coords.x<500 && coords.z<500) { //controla que els arbres estiguin dins les mides del mapa
+                        Instantiate(Resources.Load(PrefabResource, typeof(GameObject)), coords, Quaternion.identity); //Crear l'arbre a la posició coords i amb el model de arbolYamayo1
+                    }
+                k = k + 2;
+                }
+            }
+
+        }
+
+    }
+}
