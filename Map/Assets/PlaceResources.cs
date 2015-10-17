794f2a71eb3cc3174ec71b3654f934b60a76249c
sseerr20 <jojomasira@gmail.com>
2015-10-17 11:35:45 +0200

    Ja s'instancien GameObjects des de l'script

---
+++ Map/Assets/PlaceResources.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaceResources : MonoBehaviour {
+
+    // Use this for initialization
+    Vector3 position = new Vector3(95, 2.5f, 160f);
+    Vector3 position2 = new Vector3(85, -1.650108f, 166.89f);
+    void Start () {
+        GameObject tree = Instantiate(Resources.Load("arbolYamato1", typeof(GameObject)), position, Quaternion.identity) as GameObject ;
+        GameObject Tree2 = Instantiate(Resources.Load("arbolYamato1", typeof(GameObject)), position2, Quaternion.identity) as GameObject;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
