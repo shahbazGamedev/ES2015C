c48bc84eeb0dc5be2ccb81aa0c002c2807005034
sseerr20 <jojomasira@gmail.com>
2015-10-17 13:53:34 +0200

    Genera arbres aleatoriament per el mapa

--- Map/Assets/PlaceResources.cs
+++ Map/Assets/PlaceResources.cs
@@ -4,11 +4,17 @@ using System.Collections;
 public class PlaceResources : MonoBehaviour {
 
     // Use this for initialization
-    Vector3 position = new Vector3(95, 2.5f, 160f);
-    Vector3 position2 = new Vector3(85, -1.650108f, 166.89f);
+    //Vector3 position = new Vector3(95, 2.5f, 160f);
+    //Vector3 position2 = new Vector3(85, -1.650108f, 166.89f);
+    private int i;
+    int width = 500;
+    int depth = 500;
+    Vector3 coords;
+    //double rDouble = r.NextDouble() * range;
     void Start () {
-        GameObject tree = Instantiate(Resources.Load("arbolYamato1", typeof(GameObject)), position, Quaternion.identity) as GameObject ;
-        GameObject Tree2 = Instantiate(Resources.Load("arbolYamato1", typeof(GameObject)), position2, Quaternion.identity) as GameObject;
+        PlaceRandomTrees(100,"arbolYamato1");
+        //
+        //
 
     }
 	
@@ -16,4 +22,14 @@ public class PlaceResources : MonoBehaviour {
 	void Update () {
 	
 	}
+
+    void PlaceRandomTrees(int numberOfAsset,string asset) {
+        for (i=0;i<numberOfAsset;i++) {
+            double randomDepth = Random.Range(0, depth);
+            double randomWidth = Random.Range(0, width);
+            coords = new Vector3((float)randomWidth,-1.6f,(float)randomDepth);
+            Instantiate(Resources.Load(asset, typeof(GameObject)), coords, Quaternion.identity);
+
+        }
+    }
 }
