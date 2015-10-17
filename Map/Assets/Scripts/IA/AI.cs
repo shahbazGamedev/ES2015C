93d80cd68a8de4090d10a8668bb23c871879a6d2
sseerr20 <jojomasira@gmail.com>
2015-10-17 18:54:38 +0200

    -Afegits més atributs a la classe I.A -Afegida documentació -Creats tags pels edificis

--- Map/Assets/Scripts/IA/AI.cs
+++ Map/Assets/Scripts/IA/AI.cs
@@ -7,37 +7,32 @@ public class AI : MonoBehaviour {
 
     // Use this for initialization
     List<Unit> civils;
-    //List<GameObject> townCenters;
     int townCenters;
     List<GameObject> soldiers;
     Vector3 position;
     Unit civil;
-    bool building=false;
-    int i;
+
+    private int food, metal, wood;
+    private bool building=false;
+    private int i = 0;
 
     void Start () {
-        position = new Vector3(110,0,200);
-        //civil= Instantiate(Resources.Load("yamato_civil", typeof(GameObject)), position, Quaternion.identity) as GameObject;
-        civil = Instantiate(Resources.Load("yamato_civil", typeof(GameObject)), position, Quaternion.identity) as Unit;
-        civils.Add(civil);
     }
 	
 	// Update is called once per frame
 	void Update () {
-        buildTownCenter();
-        build("farm","food");
-        build("mine", "metal");
-        build("woodCutter", "tree");
     }
 
 
-    void buildTownCenter() {
+    private void buildTownCenter() {
         //Si no tinc cap centro urbano, tenir-ne una ha de ser la meva prioritat
-        if (townCenters == 0) {
+        if (townCenters == 0 && food==100 && wood==100) { //Es comproven els recursos
 
             while ( i < civils.Count && building == true) // Loop with for.
             {
                 // civilian.buildTownCenter();  //Encara no està implementat
+                food = food - 100;
+                wood = wood - 100;
                 townCenters++;
                 building = true;
             }
@@ -46,44 +41,94 @@ public class AI : MonoBehaviour {
         }
     }
 
-    void build(String building, String resource) {
+    private void build(String building) {
 
-        GameObject food;
-        GameObject farm;
+        GameObject resourceGameObject;
+        GameObject buildingGameObject;
         GameObject townCenter;
         Vector3 buildingPosition;
         Vector3 unitPosition;
         Vector3 closestDistance;
+        String resource;
         float totalDist;
+
+        switch (building) {
+            case "farm":
+                resource = "food";
+                break;
+            case "woodCutter":
+                resource = "wood";
+                break;
+            case "mine":
+                resource = "metal";
+                break;
+            default:
+                resource = null;
+                break;
+
+        }
 
         foreach (Unit civilian in civils)
         {
             unitPosition = civilian.transform.position; //Agafo la posicio del civil
             townCenter = civilian.FindClosest("townCenter"); //Retorna el TownCenter més proper
             buildingPosition = townCenter.transform.position;//Agafo la posicio
-            closestDistance = new Vector3(unitPosition.x - buildingPosition.x,0,unitPosition.z - buildingPosition.z);
-            totalDist = (float)Math.Sqrt(closestDistance.x*closestDistance.x+closestDistance.z*closestDistance.z);//i calculo  la distancia euclidania
-
-            if (totalDist > 50) { //Si les distancia és més petita de 50 no val la pena anar a construir una farm, ja es pot anar al TownCenter
-                farm = civilian.FindClosest(building);
-                buildingPosition = farm.transform.position;
-                closestDistance = new Vector3(unitPosition.x - buildingPosition.x, 0, unitPosition.z - buildingPosition.z);
-                totalDist = (float)Math.Sqrt(closestDistance.x * closestDistance.x + closestDistance.z * closestDistance.z);//i calculo  la distancia euclidania
-
-                if (totalDist>50) {
-                    food = civilian.FindClosest(resource);
-                    buildingPosition = food.transform.position;
+            closestDistance = new Vector3(unitPosition.x - buildingPosition.x, 0, unitPosition.z - buildingPosition.z);
+            totalDist = (float)Math.Sqrt(closestDistance.x * closestDistance.x + closestDistance.z * closestDistance.z);//i calculo  la distancia euclidania
+            if (wood == 50 && food == 100)
+            {
+                if (totalDist > 50)
+                { //Si les distancia és més petita de 50 no val la pena anar a construir una farm, ja es pot anar al TownCenter
+                    buildingGameObject = civilian.FindClosest(building);
+                    buildingPosition = buildingGameObject.transform.position;
                     closestDistance = new Vector3(unitPosition.x - buildingPosition.x, 0, unitPosition.z - buildingPosition.z);
                     totalDist = (float)Math.Sqrt(closestDistance.x * closestDistance.x + closestDistance.z * closestDistance.z);//i calculo  la distancia euclidania
 
-                    if (totalDist < 50) {
-                        //civilian.buildFarm();        Encara no esta implementat aquest mètode
+                    if (totalDist > 50)
+                    {
+                        resourceGameObject = civilian.FindClosest(resource);
+                        buildingPosition = resourceGameObject.transform.position;
+                        closestDistance = new Vector3(unitPosition.x - buildingPosition.x, 0, unitPosition.z - buildingPosition.z);
+                        totalDist = (float)Math.Sqrt(closestDistance.x * closestDistance.x + closestDistance.z * closestDistance.z);//i calculo  la distancia euclidania
+
+                        if (totalDist < 50)
+                        {
+                            //civilian.build(building);        Encara no esta implementat aquest mètode
+                            wood = wood - 50;
+                            food = food - 50;
+                        }
+
                     }
-
                 }
-            }
 
+            }
         }
     }
+
+    public void incFood(int num) {
+        food = food +num;
+    }
+
+    public void incWood(int num) {
+        wood = wood + num;
+    }
+
+    public void incMetal(int num) {
+        metal = metal+num;
+    }
+
+    public int getFood() {
+        return food;
+    }
+
+    public int getWood()
+    {
+        return wood;
+    }
+
+    public int getMetal() {
+        return metal;
+    }
+
 
 }
