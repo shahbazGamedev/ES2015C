32e1599b13680fa4dd873cafb9c9912eef2e8b03
sseerr20 <jojomasira@gmail.com>
2015-11-01 14:48:23 +0100

    - Per que es vegui al minimapa s'han d'afegir les boles de colors alrs prefabs

--- Map/Assets/Scripts/MiniMapScript.cs
+++ Map/Assets/Scripts/MiniMapScript.cs
@@ -9,49 +9,38 @@ public class MiniMapScript : MonoBehaviour {
     List<GameObject> miniMapObjects; //Es objectes que estan dins del minimapa
     public GameObject redColor; //El que es mostrara al minimapa
     public GameObject blueColor; //El que es mostrara al minimapa
-    public GameObject cylinder;//testeig
     private Vector3 unitPosition;
     private float nextActionTime = 0.0f;
     public float period = 0.5f;
+    Vector3 pos = new Vector3(0, 0f, 0);
 
     // Use this for initialization
     public void Start() {
-        miniMapObjects = new List<GameObject>();
-        trackedBlueObjects = new List<GameObject>();
-        trackedRedObjects = new List<GameObject>();
-        addAllyGameObject(cylinder);//testeig
-        /*Vector3 coords = new Vector3((float)120f, 0f, (float)150f);
-        Object o = Instantiate(Resources.Load("yamato_civil", typeof(GameObject)), coords, Quaternion.identity);
-        addAllyGameObject((GameObject)o);*/
-        createMiniMapObjects();
+        Vector3 coords = new Vector3(114f, 0f, 134f);
+        Transform civil = Instantiate(Resources.Load("yamato_civil"), coords, Quaternion.identity) as Transform;
+        //redColor = new GameObject();
+        createMiniMapRed((civil));
+        //addAllyGameObject((GameObject)o);
 
     }
 
     // Update is called once per frame
     void Update() {
-        if (Time.time > nextActionTime)
-        {
-            nextActionTime += period;
-            UpdateMap();
-        }
-       // UpdateMap();
     }
 
-    void createMiniMapObjects()
+
+
+    void createMiniMapRed(Transform go)
     {
+        GameObject redColor = Instantiate(Resources.Load("MinimapRed"),pos, Quaternion.identity) as GameObject;
+        redColor.transform.parent = go;
+    }
 
-        foreach (GameObject o in trackedRedObjects)
-        { //Anem recorrent la llista dels objectes que volem que sortin al minimapa
-            GameObject k = Instantiate(redColor, o.transform.position, Quaternion.identity) as GameObject; //A la posicio de l'objecte hi afegim el color vermellS
-            miniMapObjects.Add(k);
-        }
-
-        foreach (GameObject o in trackedBlueObjects)
-        { //Anem recorrent la llista dels objectes que volem que sortin al minimapa
-            GameObject k = Instantiate(blueColor, o.transform.position, Quaternion.identity) as GameObject; //A la posicio de l'objecte hi afegim el color blau
-            miniMapObjects.Add(k);
-        }
-
+    void createMiniMapBlue(Transform go)
+    {
+        GameObject k = Instantiate(blueColor, go.transform.position, Quaternion.identity) as GameObject;
+        //var go = Instantiate(prefabEffect) as GameObject;
+        k.transform.parent = go.transform;
     }
 
     void addEnemyGameObject(GameObject go) {
