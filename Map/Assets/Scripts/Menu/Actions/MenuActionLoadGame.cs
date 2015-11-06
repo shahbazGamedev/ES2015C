b7ec028bf9ab92ae0c75536d30091154cb19e0df
joanbm <joanbrugueram@gmail.com>
2015-11-06 17:27:51 +0100

    Implement main menu options.

---
+++ Map/Assets/Scripts/Menu/Actions/MenuActionLoadGame.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+/// <summary>
+/// Loads a previously saved game.
+/// </summary>
+public class MenuActionLoadGame : MonoBehaviour
+{
+    /// <summary>
+    /// The dropdown that contains the list of saved games.
+    /// </summary>
+    public Dropdown savedGameDropdown;
+
+    public void LoadGame()
+    {
+        // Check correct association
+        if (savedGameDropdown == null)
+        {
+            Debug.LogError("Saved Game dropdown not associated.");
+            return;
+        }
+
+        // Get selected slot no.
+        Dropdown dropDownCampaign = savedGameDropdown.GetComponent<Dropdown>();
+        int selectedSavedGame = dropDownCampaign.value;
+
+        Debug.LogError("Load Game not implemented: Slot " + selectedSavedGame);
+    }
+}
---
+++ Map/Assets/Scripts/Menu/Actions/MenuActionStartGame.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+/// <summary>
+/// Starts a new game.
+/// </summary>
+public class MenuActionStartGame : MonoBehaviour
+{
+    /// <summary>
+    /// The dropdown that contains the list of campaigns.
+    /// </summary>
+    public Dropdown campaigsDropdown;
+
+    public void StartGame()
+    {
+        // Check correct association
+        if (campaigsDropdown == null)
+        {
+            Debug.LogError("Campaigns dropdown not associated.");
+            return;
+        }
+
+        // All scenes after the main menu are the campaign: 1 is the first campaign, 2 is the second, etc.
+        Application.LoadLevel(1+campaigsDropdown.value);
+    }
+}
