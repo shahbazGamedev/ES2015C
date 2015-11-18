85a8e24a93db3cc8a550caf7dc33e8eb549def3c
Ruben <Ruben@Lenovo.local>
2015-11-18 01:04:29 +0100

    Integración scripts IA

--- Map/Assets/Scripts/Player/UserInput.cs
+++ Map/Assets/Scripts/Player/UserInput.cs
@@ -114,13 +114,9 @@ public class UserInput : MonoBehaviour
             }
             else if (rightClick && player.SelectedObject != null && player.SelectedObject.IsOwnedBy(player))
             {
-
-				if (player.SelectedObject.tag == "civil")
-				{
-					player.SelectedObject.GetComponent<CivilUnit>().harvesting=false;
-					if (player.SelectedObject.CanBuild()) {
-						player.SelectedObject.GetComponent<CivilUnit>().building=false;
-					}
+                player.SelectedObject.GetComponent<CivilUnit>().harvesting=false;
+				if (player.SelectedObject.CanBuild()) {
+					player.SelectedObject.GetComponent<CivilUnit>().building=false;
 				}
 				//Atacar
                 if (player.SelectedObject.CanAttack() &&
@@ -142,7 +138,7 @@ public class UserInput : MonoBehaviour
 				}
                 
                 //Recolecto
-                else if (player.SelectedObject.tag == "civil" && targetRtsElement != null && targetRtsElement.tag == "wood")
+				else if (player.SelectedObject.tag == "civil" && targetRtsElement != null && targetRtsElement.GetComponent<Resource>()) //tag == "wood"
 				{
                     //player.SelectedObject.MoveTo(hit.point);
                     player.SelectedObject.GetComponent<CivilUnit>().StartHarvest(targetRtsElement.GetComponent<Resource>());//, Building store)
