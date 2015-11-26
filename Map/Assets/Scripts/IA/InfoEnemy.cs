9d1197f7d1080ff8123dd0e1e3f6b40644084f4b
ftorraba7 <fernando.torralba.barrabes@gmail.com>
2015-11-26 23:14:34 +0100

    Creado Script para hacer aparecer la tabla con los datos del Enemy.

---
+++ Map/Assets/Scripts/IA/InfoEnemy.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InfoEnemy : MonoBehaviour
+{
+
+	private GameObject menu;
+	private bool activated = true;
+	// Use this for initialization
+	//GameObject prefab;
+	void Start () {
+		GameObject menu;
+	}
+	
+	
+	private void CreateMenu() {
+        //menu = Instantiate(Resources.Load("HUDInfoEnemy")) as GameObject;     
+    }
+
+	void Update() {
+
+		if (activated) {
+			if (Input.GetKey (KeyCode.X)) {
+				CreateMenu();
+			}
+		}
+
+		else {
+			if (Input.GetKey (KeyCode.X)) {
+				menu.active = false;
+			}
+		}
+	}
+	/*
+	public void OnDisable ()
+	{
+		if (guiObj != null)
+			GameObject.DestroyImmediate (guiObj.gameObject);
+	}	
+*/
+}
