51d1e6339b899ed1c8beb0eda179d887f8f871da
saragarciacano <garciacanosara@gmail.com>
2015-11-29 14:21:32 +0100

    Añado instanciaciones que faltan
    Peticion del teamB

---
+++ Map/Assets/Scripts/RTSObject/Building/Persian/Persian_Academy.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Persian_Academy : Building {
+
+    /*** Metodes per defecte de Unity ***/
+
+	protected override void Awake()
+    {
+		base.Awake();
+		objectName = "Persian Academy";
+		cost = 400;
+		hitPoints = maxHitPoints = 800;
+        baseDefense = 5;
+    }
+}
