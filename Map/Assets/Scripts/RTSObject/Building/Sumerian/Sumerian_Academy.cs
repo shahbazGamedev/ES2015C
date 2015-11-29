6a0c28fd2341b578f902744214a8eecb3051b0ec
saragarciacano <garciacanosara@gmail.com>
2015-11-29 14:21:32 +0100

    Añado instanciaciones que faltan
    Peticion del teamB

---
+++ Map/Assets/Scripts/RTSObject/Building/Sumerian/Sumerian_Academy.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Sumerian_Academy : Building {
+
+    /*** Metodes per defecte de Unity ***/
+
+	protected override void Awake()
+    {
+		base.Awake();
+		objectName = "Sumerian Academy";
+		cost = 400;
+		hitPoints = maxHitPoints = 800;
+        baseDefense = 5;
+    }
+}
