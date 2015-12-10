0cde96bb8842026a8d207ed631ced621db3fa523
ftorraba7 <fernando.torralba.barrabes@gmail.com>
2015-12-10 22:34:27 +0100

    Comienza a funcionar el control del volumen, falta instanciarlo correctamente al iniciar el juego. (Desde Terrain)

--- Map/Assets/Scripts/IA/VolumeControl.cs
+++ Map/Assets/Scripts/IA/VolumeControl.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class VolumeControl : MonoBehaviour
@@ -11,13 +12,23 @@ public class VolumeControl : MonoBehaviour
 
 	// Use this for initialization
 	//GameObject prefab;
+
+	public AudioSource ambiente;
+
 	void Start () {
+		ambiente = gameObject.AddComponent<AudioSource>();
+		ambiente.clip = Resources.Load("Sounds/music1") as AudioClip;
+		ambiente.volume = 0.5f;
+		ambiente.Play();
 		GameObject menuVol;
 		//artificialIntelligence = GameObject.Find("EnemyPlayer1").GetComponent<Player>();
         //civilitzation = GameObject.Find("EnemyPlayer1").GetComponent<Player>().civilization;
 	}
 	
-	
+	public void PonVolumen (float valor) {
+		ambiente.volume = valor/20f; //divido 20 porque hemos asginado que va entre 0 y 20
+	}
+
 	private void CreateMenuVC() {
         menuVol = Instantiate(Resources.Load("ControlVolumen")) as GameObject;     
     }
