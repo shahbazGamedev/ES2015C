c582e991259a2241b017fbcc0d4ade8842a24a5a
ftorraba7 <fernando.torralba.barrabes@gmail.com>
2015-12-10 21:11:01 +0100

    Press[V] y se muestra por pantalla el menu de control de Volumen

---
+++ Map/Assets/Scripts/IA/VolumeControl.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VolumeControl : MonoBehaviour
+{
+
+	private GameObject menuVol;
+	private bool activated = false;
+	//public Player artificialIntelligence;
+	//public PlayerCivilization civilitzation;
+
+	// Use this for initialization
+	//GameObject prefab;
+	void Start () {
+		GameObject menuVol;
+		//artificialIntelligence = GameObject.Find("EnemyPlayer1").GetComponent<Player>();
+        //civilitzation = GameObject.Find("EnemyPlayer1").GetComponent<Player>().civilization;
+	}
+	
+	
+	private void CreateMenuVC() {
+        menuVol = Instantiate(Resources.Load("ControlVolumen")) as GameObject;     
+    }
+
+	void Update() {
+
+		if (!activated) {
+			if (Input.GetKey (KeyCode.V)) {
+				activated=true;
+				CreateMenuVC();
+
+			}
+		}
+
+		else {
+			if (Input.GetKey (KeyCode.V)) {
+				activated=false;
+				menuVol.active = false;
+			}
+		}
+	}
+	
+}
