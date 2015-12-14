81c71b83f8eda07401351311bcb7a20c442b8f00
ftorraba7 <fernando.torralba.barrabes@gmail.com>
2015-12-14 17:30:39 +0100

    Se sube nueva version de sonido, se arregla en breves!

--- Map/Assets/Scripts/IA/VolumeControlAccess.cs
+++ Map/Assets/Scripts/IA/VolumeControlAccess.cs
@@ -13,19 +13,31 @@ public class VolumeControlAccess : MonoBehaviour
 	// Use this for initialization
 	//GameObject prefab;
 
+	
+	
 	public AudioSource ambiente;
+	VolumeControl volCont;
+
+	public float auxvolumen;
 
 
 	void Start () {
 
+		ambiente = gameObject.AddComponent<AudioSource>();
+		ambiente.clip = Resources.Load("Sounds/music1") as AudioClip;
+		ambiente.volume = 0.5f;
+		ambiente.Play();
+		GameObject menuVol;
 
+		volCont = GameObject.Find("ControlVolumen").GetComponent<VolumeControl>();
+        auxvolumen = (float) volCont.ambiente2.volume;
 	}
 	
 
 	public void CreateMenuVC() {
         menuVol = Instantiate(Resources.Load("ControlVolumen")) as GameObject;  
 
-    }
+
     /*
     public void setAmbiente(AudioSource aux, int aaa) {
     	//VolumeControl vc = GetComponent<VolumeControl>();
@@ -33,13 +45,17 @@ public class VolumeControlAccess : MonoBehaviour
         //ambiente.volume = aux.volume; 
         //xxx = aaa;
     }*/
+    }
+	public void Update() {
 
-	void Update() {
+		auxvolumen = (float) volCont.ambiente2.volume;
+		ambiente.volume = auxvolumen;
 
 		if (!activated) {
 			if (Input.GetKey (KeyCode.V)) {
 				activated=true;
 				CreateMenuVC();
+				menuVol.active = true;
 
 			}
 		}
